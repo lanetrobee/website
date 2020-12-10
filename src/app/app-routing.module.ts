4e75f483686d30c862cb9c050f37098efc825c29
Lane <40241524+lanetrobee@users.noreply.github.com>
2020-12-09 22:36:06 -0600

    change to gh action (#3)
    * deploy script dist fix
    
    * moving assests around
    
    * adding header tag to index
    
    * adding components
    
    * fixed path to assest for mario project
    
    * adding routing mario and landing page
    
    * fixes :)
    
    * moving styles to css
    
    * minor changes
    
    * updating gh actions
    
    * reverting changes to gh actions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './features/landing/landing.component';
+import { MarioComponent } from './projects/mario/mario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing-component', component: LandingComponent },
+  { path: 'mario-component', component: MarioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
